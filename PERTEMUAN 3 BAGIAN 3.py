e82f7a82cc001ed6213b64bd3222f8aae534b668
Aria Dewangga <ariadewangga098@gmail.com>
2025-10-08 11:39:15 +0700

    Add files via upload

---
+++ PERTEMUAN 3 BAGIAN 3.py
@@ -0,0 +1,9 @@
+list_buah=['Mangga','Jambu','Jeruk','Jambu']
+print(list_buah)
+# hapus item pertama dengan nilai 'Jambu'
+list_buah.remove('Jambu')
+print(list_buah)
+del list_buah[1]
+print(list_buah)
+del list_buah[0:2]
+print(list_buah)
